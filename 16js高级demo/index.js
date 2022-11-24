15d3e0bdb4b36b0fcb7a84feff74b6da70afd828
zhangqiang <44993003+JACK-ZHANG-coming@users.noreply.github.com>
2022-11-25 00:36:23 +0800

    update

--- 16js高级demo/index.js
+++ 16js高级demo/index.js
@@ -1,5 +1,29 @@
-console.log(a)
-{
-  var a = 1
+// console.log(a)
+// {
+//   var a = 1
+// }
+// console.log(a)
+
+//#region 00 new.target
+function King(){
+  if(!new.target){
+    throw 'King must be instantiated using "new"'
+  }
+  console.log('King instantiated using "new"',new.target);
 }
-console.log(a)
+// King();
+new King();
+
+//#endregion
+
+//#region 01 js中this、apply、call、bind 之间的理解与使用
+
+//#endregion
+
+
+//#region 02 执行上下文栈
+if(!(b in window)){
+  var b = 1;
+}
+console.log(b);
+//#endregion
---
+++ 21node.js学习巩固/helloNodejs.js
@@ -0,0 +1,2 @@
+console.log('hello nodejs')
+console.log(__filename) // 当前文件完整路径
